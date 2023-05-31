9c51c7f2642f429b3af4f81b1d3de72b2ef83737
Odey John Ebinyi <odeyjhn@gmail.com>
2023-05-31 16:15:09 -0400

    Caesar cipher

--- cc_utils.c
+++ cc_utils.c
@@ -72,35 +72,3 @@ void read_char(const char* filename, char *message)
 		exit(EXIT_FAILURE);
 	}
 }
-
-/*
- * Copy string src to buffer dst of size dsize. At most dsize-1
- * chars will be copied. Always NUL terminates (unless dsize == 0).
- * Returns strlen(src); if retval >= dsize, truncation occured.
- */
-size_t strlcpy(char * __restrict dst, const char *__restrict src, size_t dsize)
-{
-	const char *osrc = src;
-	size_t nleft = dsize;
-	
-	/* Copy as many bytes as will fit. */
-	if(nleft != 0)
-	{
-		while(--nleft != 0)
-		{
-			if((*dst++ = *src++) == '\0')
-				break;
-		}
-	}
-	
-	/* Not enough room in dst, add NUL and traverse rest of src. */
-	if(nleft == 0)
-	{
-		if(dsize != 0)
-			*dst = '\0';
-		while(*src++)
-			;
-	}
-	
-	return(src - osrc - 1); /* count does not include NUL */
-}
